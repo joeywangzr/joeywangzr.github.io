ab8d8880f27e3cafd1d9166fde277195308cf5be
Joey Wang <joeywangzr@gmail.com>
2022-02-23 13:48:32 -0700

    heroku changes

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from './OrbitControls.js'
 
+// Heroku
+var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+server.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
+
 // THREEX.DOMEVENTS.JS
 
 /** @namespace */
