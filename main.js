94b022e1da210d9e28e229a504c9606594172e72
Joey Wang <joeywangzr@gmail.com>
2022-02-23 13:51:50 -0700

    heroku test

--- main.js
+++ main.js
@@ -9,10 +9,9 @@ import * as THREE from 'three'
 import { OrbitControls } from './OrbitControls.js'
 
 // Heroku
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
 });
 
 // THREEX.DOMEVENTS.JS
