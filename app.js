7d157b0ef3fa9a7f442b8c0abd67b3c691165850
Joey Wang <joeywangzr@gmail.com>
2022-02-23 21:36:37 -0700

    optimize for heroku

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.use(express.static(__dirname + '/public'))
 app.use('/build/', express.static(path.join(__dirname, 'node_modules/three/build')))
 app.use('/jsm/', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')))
 
-app.listen(3000, () => console.log('Visit http://127.0.0.1:3000'))
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
