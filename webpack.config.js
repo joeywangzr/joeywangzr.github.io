a9b2a1bb1619876c6e4a245822fd1e4bd97ddaeb
Joey Wang <joeywangzr@gmail.com>
2022-04-27 22:10:19 -0600

    improved light mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname),
+    path: path.resolve(__dirname + '/javascript'),
     filename: 'main.js',
   },
   module: {
