8dea1b151e43862969f574792d3df87674e1f7b2
Joey Wang <joeywangzr@gmail.com>
2022-09-02 16:51:02 -0600

    restore old

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './js/index.js',
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname + '/webpack'),
+    path: path.resolve(__dirname + '/javascript'),
     filename: 'main.js',
   },
   module: {
